a7bc6c30b056b1134f93a32fd69c4f3bf036a778
John00708 <john.andrews007@gmail.com>
2014-06-14 19:28:41 +0300

    Some small programming spelling fixes

--- Watch Dogs/src/altitude/source/code/Main.java
+++ Watch Dogs/src/altitude/source/code/Main.java
@@ -1,5 +1,6 @@
 package altitude.source.code;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -7,16 +8,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin{
 	@Override
 	public void onEnable() {
-		getLogger().info("Watch-Dogs v1 has been enabled!") // Showing when the plugin has been enabled
+		getLogger().info("Watch-Dogs v1 has been enabled!"); // Showing when the plugin has been enabled
 	}
 	@Override
 	public void onDisable() {
-		getLogger().info("Watch-Dogs v1 has been disabled!") // Showing when the plugin has been disabled
+		getLogger().info("Watch-Dogs v1 has been disabled!"); // Showing when the plugin has been disabled
 	}
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("watchtest")) { // Adding a dummy command
-			sender.sendMessage(ChatColor.AQUA + "The dummy command works!") // Sending an Aqua Message back to test if it works
+			sender.sendMessage(ChatColor.AQUA + "The dummy command works!"); // Sending an Aqua Message back to test if it works
 			return true;
 		}
 		return false; 
