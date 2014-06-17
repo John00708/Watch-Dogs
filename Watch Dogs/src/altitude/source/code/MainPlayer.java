d33da6b31ceeab1c2237e15d01874949defc6968
nathan Siler <allen.n.siler@gmail.com>
2014-06-17 19:57:26 -0400

    Finished up all we need right now for the player class

--- Watch Dogs/src/altitude/source/code/MainPlayer.java
+++ Watch Dogs/src/altitude/source/code/MainPlayer.java
@@ -2,4 +2,41 @@ package altitude.source.code;
 
 public class MainPlayer {
 	//This is the class for Nathan to work on
+	private String name;
+	private int health;
+	private double money;
+	
+	public Player(String n) {
+		name = n;
+		health = 10;
+		money = 19.99;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getHealth() {
+		return health;
+	}
+	
+	public double getMoney() {
+		return money;
+	}
+	
+	public void setHealth(int h) {
+		health = h;
+	}
+	
+	public void setMoney(double m) {
+		moeny = m;
+	}
+	
+	public void modifyHealth (int h) {
+		health += h;
+	}
+	
+	public void modifyMoney(double m) {
+		money += m;
+	}
 }
