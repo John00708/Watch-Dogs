2b1b3e5807474a53437d2873dac9bc0aa71a065f
nathan Siler <allen.n.siler@gmail.com>
2014-06-18 18:05:07 -0400

    Update Enemy.java

--- Watch Dogs/src/altitude/source/code/Enemy.java
+++ Watch Dogs/src/altitude/source/code/Enemy.java
@@ -1,5 +1,15 @@
 package altitude.source.code;
 
-public class Enemy {
-
+public class Enemy extends Entity {
+  
+  privatae int level;
+  
+  public Monster(int level) {
+    setHealth(level * 2);
+    
+  }
+  
+  public int getLevel() {
+    return level;
+  }
 }
