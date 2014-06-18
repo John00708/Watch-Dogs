953125fbdc7a4d350f4f5537a50a66337e0863a6
nathan Siler <allen.n.siler@gmail.com>
2014-06-17 20:11:45 -0400

    Update MainCampaign.java

--- Watch Dogs/src/altitude/source/code/MainCampaign.java
+++ Watch Dogs/src/altitude/source/code/MainCampaign.java
@@ -2,4 +2,25 @@ package altitude.source.code;
 
 public class MainCampaign {
 	// This is the class that we can make the campaign in
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		print("What is your name?");
+		
+		String name = s.nextLine();
+		
+		Player player = new Player(name);
+		
+		print("Welcome to Watch_Blocks, " + name + "!");
+		
+		int input;
+		
+		do {
+			System.out.prinln("[0] Quit. ");
+			System.out.println("[1] Shoot Maurice.");
+			System.out.println("[2] Sneak past the Police.");
+			System.out.println("[3] Hack the camera to unlock the door.");
+			System.out.println("[2] Lure the cops away");
+		}
+	}
 }
