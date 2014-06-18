f8ca6b34eec50b4a6f4923b04442ddadc685a843
nathan Siler <allen.n.siler@gmail.com>
2014-06-18 17:58:44 -0400

    Update Entity.java

--- Watch Dogs/src/altitude/source/code/Entity.java
+++ Watch Dogs/src/altitude/source/code/Entity.java
@@ -1,5 +1,18 @@
 package altitude.source.code;
 
 public class Entity {
+  
+  private int health;
+  
+  public int getHealth() {
+    return health;
+  }
+  
+  public void setHealth(int h) {
+    health = h;
+  }
+  public void modifyHealth(int h) {
+    health += h;
+  }
 
 }
