a6b995b07d84ca277034dfd5731cbac3201ed361
nathan Siler <allen.n.siler@gmail.com>
2014-06-20 21:00:32 -0400

    Update MainCampaign.java

--- Watch Dogs/src/altitude/source/code/MainCampaign.java
+++ Watch Dogs/src/altitude/source/code/MainCampaign.java
@@ -3,31 +3,21 @@ package altitude.source.code;
 import java.util.Scanner;
 
 import org.bukkit.entity.Player;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
 
-public class MainCampaign {
-	// This is the class that we can make the campaign in
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		
-		print("What is your name?");
-		
-		String name = s.nextLine();
-		
-		Player player = new Player(name);
-		
-		print("Welcome to Watch_Blocks, " + name + "!");
-		
-		int input;
-		
-		do {
-			System.out.println("[0] Quit. ");
-			System.out.println("[1] Shoot Maurice.");
-			System.out.println("[2] Sneak past the Police.");
-			System.out.println("[3] Hack the camera to unlock the door.");
-			System.out.println("[2] Lure the cops away");
-			//Actions? "1" System.out.read("1") then do some thing? -Ice 
-			//Didn't know were you guys were at on this currently.
-			//well i was just mapping out some of the things we would have to do later in the future ~voracious
+public class MainCampaign extends JavaPlugin{
+	
+	public void onDisable() {
+		PluginDescriptionFile pdffile = this.getDescription();
+		this.logger.info(pdffile.getName() + "Has Been Disabled!");
+	}
+	
+	public void onEnable() {
+		PluginDescriptionFile pdffile = this.getDescription();
+		this.logger.info(pdffile.getName() + " Version " + pdffile.getVersion() + " Has Been Enabled!");
+	}
+	
 		}
 	}
 }
