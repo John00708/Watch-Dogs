16ea9d37f850df8de42e6f9a2cc8b1b487694b5e
nathan Siler <allen.n.siler@gmail.com>
2014-06-23 19:14:31 -0400

    YAY!!! i finally accomplished some stuff xD ~Voracious

--- Watch Dogs/src/altitude/source/code/SkillTree.java
+++ Watch Dogs/src/altitude/source/code/SkillTree.java
@@ -1,5 +1,28 @@
 package altitude.source.code;
 
-public class SkillTree {
+import java.util.logging.logger;[]
+
+public class SkillTree extends JavaPlugin{
+  public final Logger logger = Logger.getLogger("Minecraft");
+  public static Watch-Dogs plugin;
+  
+  public void onDisable() {
+    PluginDescriptionFile pdffile = this.getDescription();
+    this.logger.info(pdffile.getName() + " Has Been Disabled!");
+  }
+  
+  public void onEnable() {
+    PluginDescriptionFile pdffile = this.getDescription();
+    this.logger.info(pdffile.getName() + " Has Been Enabled!");
+    
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+      Player player = (Player) sender;
+      if(commandLabel.equalsIgnoreCase("st")){
+        if(args.length == 0){
+          player.sendMessage(ChatColor.DARK_RED + "/sk <Skill>");
+        }
+      }
+    }
+    
 
 }
