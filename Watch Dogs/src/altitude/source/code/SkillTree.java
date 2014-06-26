3f730128a564e7580af5a623990322cb55eade4a
nathan Siler <allen.n.siler@gmail.com>
2014-06-25 20:32:47 -0400

    Update SkillTree.java

--- Watch Dogs/src/altitude/source/code/SkillTree.java
+++ Watch Dogs/src/altitude/source/code/SkillTree.java
@@ -1,35 +1,17 @@
 package altitude.source.code;
 
-import java.util.logging.Logger;
-import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
-public class SkillTree extends JavaPlugin{
+public class SkillTree {
   public final Logger logger = Logger.getLogger("Minecraft");
   public static Main plugin;
+  private Inventory inv;
+  private ItemStack c, s, a;
   
-    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-      Player player = (Player) sender;
-      if(commandLabel.equalsIgnoreCase("st")){
-      	if(commandLabel.equalsIgnoreCase("driving")){
-      		player.sendMessage("accesing...");
-      	}else if(commandLabel.equalsIgnoreCase("hacking")){
-      		player.sendMessage("accesing...");
-      	}else if(commandLabel.equalsIgnoreCase("combat")){
-      		player.sendMessage("accesing...");
-      	}else if(commandLabel.equalsIgnoreCase("crafting")){
-      		player.sendMessage("accesing...");
-      	}else if(commandLabel.equalsIgnoreCase("weaponry")){
-      		player.sendMessage("accesing...");
-      	}
-        
-        }
-      }
-	return true;
-    }
-    
+  public Menu() {
+  	inv = Bukkit.getServer().createInventory(null, InventoryType)
+  }
+  
 
 }
