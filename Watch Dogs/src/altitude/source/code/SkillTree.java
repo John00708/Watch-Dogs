8f5a2a64155943f3b540a495192d95860e45a3ac
MrShrimpy <minecraftpros1225@gmail.com>
2014-07-11 17:12:35 -0600

    Updated some weird Git changes

--- Watch Dogs/src/altitude/source/code/SkillTree.java
+++ Watch Dogs/src/altitude/source/code/SkillTree.java
@@ -1,32 +1,11 @@
-<<<<<<< HEAD
-package altitude.source.code;å
+package altitude.source.code;
 
 public class SkillTree extends JavaPlugin{
 	
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
       Player player = (Player) sender;
       if(commandLabel.equalsIgnoreCase("skill")){
-        player.sendMessage(ChatColor.DARK_RED + "This works!å")
+        player.sendMessage(ChatColor.DARK_RED + "This works!")
         }
       }
     }
-    
-=======
-package altitude.source.code;
-
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-
-public class SkillTree {
-  public final Logger logger = Logger.getLogger("Minecraft");
-  public static Main plugin;
-  private Inventory inv;
-  private ItemStack c, s, a;
-  
-  public Menu() {
-  	inv = Bukkit.getServer().createInventory(null, InventoryType)
-  }
-  
->>>>>>> FETCH_HEAD
-
-}
