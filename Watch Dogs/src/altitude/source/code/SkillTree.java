c9ecc4fc6c897c83f21a2af6080843ee22781e77
Mali Plavi <marko.raspica@gmail.com>
2014-07-14 13:53:14 +0200

    1 Error fixed

--- Watch Dogs/src/altitude/source/code/SkillTree.java
+++ Watch Dogs/src/altitude/source/code/SkillTree.java
@@ -5,7 +5,7 @@ public class SkillTree extends JavaPlugin{
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
       Player player = (Player) sender;
       if(commandLabel.equalsIgnoreCase("skill")){
-        player.sendMessage(ChatColor.DARK_RED + "This works!")
+        player.sendMessage(ChatColor.DARK_RED + "This works!");
         }
       }
     }
