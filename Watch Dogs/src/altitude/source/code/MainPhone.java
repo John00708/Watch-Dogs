310dda060ec9b8872b841c51875b0b84b63f09a3
John00708 <john.andrews007@gmail.com>
2014-07-15 15:20:24 +0300

    Rewrote all the MainPhone class and applied the 1.7.9 build.

--- Watch Dogs/src/altitude/source/code/MainPhone.java
+++ Watch Dogs/src/altitude/source/code/MainPhone.java
@@ -1,127 +1,82 @@
 package altitude.source.code;
 
+import java.util.Arrays;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.util.Vector;
+import org.bukkit.plugin.Plugin;
 
 public class MainPhone implements Listener{
-	public MainPhone(Main plugin){
-		plugin.getServer().getPluginManager().registerEvents(this, plugin);
-	}
-	public void openGUI(Player player) {
-		Inventory inv = Bukkit.createInventory(null, 9, ChatColor.GOLD + "Hack Selector");
-		
-		ItemStack camera = new ItemStack(Material.GLASS);
-		ItemMeta cameraMeta = camera.getItemMeta(); //MetaData for camera item
-		ItemStack info = new ItemStack(Material.PAPER);
-		ItemMeta infoMeta = info.getItemMeta(); //MetaData for info item
-		
-		cameraMeta.setDisplayName(ChatColor.DARK_GREEN + "Security Camera"); //Setting the name of the block
-		camera.setItemMeta(cameraMeta);
-		
-		infoMeta.setDisplayName(ChatColor.DARK_BLUE + "Personal Info"); //Setting the name of the item
-		info.setItemMeta(cameraMeta);
-		
-		inv.setItem(3, camera); //Putting the items inside the inv
-		inv.setItem(4, info);
-		
-		player.openInventory(inv);
-	}
-	
-	@EventHandler(priority = EventPriority.HIGHEST)
-	public boolean onPlayerJoin(PlayerJoinEvent event) {
-		if(!event.getPlayer().getInventory().contains(Material.COMPASS)){
-			event.getPlayer().sendMessage(ChatColor.GREEN + "You just got your phone! Happy hacking!");
-			event.getPlayer().getInventory().addItem(new ItemStack(Material.COMPASS));
-		}else{
-			return false;
-		}
-		return false;
-	}
+	private Inventory inv;
+	private ItemStack Camera, Info, Battery, Barrier;
 	
 	
-	@EventHandler(priority = EventPriority.HIGHEST)
-	public void onPlayerInteract(PlayerInteractEvent event) {
-		Action a = event.getAction();
-		ItemStack is = event.getItem();
-		
-		if(a == Action.PHYSICAL || is == null || is.getType()==Material.AIR){
-			return;
-		}
-		if(is.getType()==Material.COMPASS);{
-			openGUI(event.getPlayer());
-		}
+	public MainPhone(Plugin p) {
+		inv = Bukkit.getServer().createInventory(null, 9, "Hack Selector");
+		ItemStack Barrier = new ItemStack(Material.FENCE);
+		ItemStack Info = new ItemStack(Material.PAPER);
+		ItemStack Camera = new ItemStack(Material.GLASS);
+		ItemStack Battery = new ItemStack(Material.REDSTONE);
+		ItemMeta Infom = Info.getItemMeta();
+		ItemMeta Barrierm = Barrier.getItemMeta();
+		ItemMeta Cameram = Camera.getItemMeta();
+		ItemMeta Batterym = Battery.getItemMeta();
+		Cameram.setDisplayName(ChatColor.GRAY + "Camera");
+		Infom.setDisplayName(ChatColor.DARK_GREEN + "Info");
+		Barrierm.setDisplayName(ChatColor.GOLD + "Barrier");
+		Batterym.setDisplayName(ChatColor.RED + "Battery");
+		Cameram.setLore(Arrays.asList(ChatColor.DARK_PURPLE + "This App allows you ", "to get into any exploited ", "ctOS Camera. Just look ", "directly at the camera and then ", "choose this app!"));
+		Infom.setLore(Arrays.asList(ChatColor.DARK_PURPLE + "This App, when clicked, ", "shows you many useful information ", "about the town!"));
+		Barrierm.setLore(Arrays.asList(ChatColor.DARK_PURPLE + "This App allows you ", "to retract or raise any exploited ", "ctOS Barriers. Just look ", "at the barrier and then", "choose this app!"));
+		Batterym.setLore(Arrays.asList(ChatColor.DARK_PURPLE + "This App shows you ", "the current battery level ", "and allows you to buy energy!"));
+		inv.setItem(2, Camera);
+		inv.setItem(4, Info);
+		inv.setItem(6, Battery);
+		inv.setItem(8, Barrier);
+		Bukkit.getServer().getPluginManager().registerEvents(this, (Plugin) this);
 	}
-	
+	public void show(Player p){
+		p.openInventory(inv);
+	}
 	@EventHandler
-	public void onInvClick(InventoryClickEvent e) {
-		if(ChatColor.stripColor(e.getInventory().getName()).equalsIgnoreCase("Hack Selector")){		
+	public void onInventoryClick(InventoryClickEvent e){
+		if(!e.getInventory().getName().equalsIgnoreCase(inv.getName())) return;
+		if(e.getCurrentItem().getItemMeta() == null) return;
+		if(e.getCurrentItem().getItemMeta().getDisplayName().contains("Camera")){
+			e.setCancelled(true);                                    //Do things with Camera
+			e.getWhoClicked().closeInventory();
+			
+		}
+		if(e.getCurrentItem().getItemMeta().getDisplayName().contains("Info")){
+			e.getWhoClicked().closeInventory();                      //Do things with Info
 			e.setCancelled(true);
-			if(e.getCurrentItem()==null || e.getCurrentItem().getType()==Material.AIR || !e.getCurrentItem().hasItemMeta());{
-				Player player = (Player) e.getWhoClicked();
-				player.closeInventory();
-				return;
-			}
+			
 		}
-		switch (e.getCurrentItem().getType()) {
-		case GLASS: //Do stuff when the look at the glass (Camera) Cursor
-	@EventHandler
-	public void onPlayerInteractBlock(PlayerInteractEvent e) {
-				 
-			 	Player player = e.getPlayer();
-			 	
-			 	player.sendMessage(ChatColor.DARK_GREEN + "When clicking the camera, you must look directly at it");
-			 	
-			 	Block targetblock = player.getTargetBlock(null, 150);
-			 	if (targetblock.getType() == Material.GLASS){
-			 		
-			 		Location beforeloc = player.getLocation();
-			 		Location location = targetblock.getLocation();
-			 		player.getWorld().spawnCreature(beforeloc, EntityType.ZOMBIE);
-			 		player.teleport(location);
-			 		boolean freeze = true;
-			 		player.setVelocity(new Vector(player.getVelocity().getX(), 0, player.getVelocity().getZ()));
-			 		public void OnPlayerMove(PlayerMoveEvent e){
-			 			if (freeze == true){
-			 				e.setCancelled(true);
-			 			}
-			 		}
-			 		
-			      }
-			 	}
-			 	
-			 	//Every thing I have tried doesn't work with the tp code
-			 
-
-
-			break;
-		case PAPER:
-			public void onPlayerClickInv(InventoryClickEvent e) {
-				
-			}
-			//Do stuff when they click the info ()
-
-			break;
+		if(e.getCurrentItem().getItemMeta().getDisplayName().contains("Battery")){
+			e.setCancelled(true);                                    //Do things with Battery
+			e.getWhoClicked().closeInventory();
+			
+		}
+		if(e.getCurrentItem().getItemMeta().getDisplayName().contains("Barrier")){
+			e.setCancelled(true);                                    //Do things with Barrier
+			e.getWhoClicked().closeInventory();
+			
 		}
 	}
-	
-	
-
-
+	@EventHandler
+	public void onPlayerInteract(PlayerInteractEvent e){
+		if (!(e.getAction() == Action.RIGHT_CLICK_BLOCK)) return;
+		show(e.getPlayer());
+	}
 }
+
